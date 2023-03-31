77b9e37a6149840161d12f07e60dfd5afa52f20f
Tsukina-7mochi <Tsukina-7mochi@users.noreply.github.com>
2023-03-31 12:11:21 +0000

    deploy: 9c33e17472f98faad9b242df376dda192faa7ea9

--- fonts/fonts.md
+++ fonts/fonts.md
@@ -6,7 +6,6 @@ Open Font License
 
 https://fonts.google.com/specimen/Kiwi+Maru?subset=japanese&noto.script=Jpan
 
-
 ## Makinas 4 Flat
 
 [License](https://moji-waku.com/mj_work_license/)
--- main.js
+++ main.js
@@ -1,2 +1,2 @@
-(()=>{globalThis.addEventListener("load",()=>{});})();
+(()=>{globalThis.addEventListener("DOMContentLoaded",()=>{globalThis.matchMedia("(prefers-color-scheme: dark)").addEventListener("change",e=>{document.querySelectorAll("div.repo-card").forEach(a=>{a.dataset.theme=e?"dark-theme":""})})});})();
 //# sourceMappingURL=main.js.map
